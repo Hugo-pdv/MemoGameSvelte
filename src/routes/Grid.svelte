016a95e823153101ba2c66930008dea9d0327eb9
Hugo Podvin <hugo54podvin@gmail.com>
2023-07-20 14:48:36 +0200

    avancement

--- src/routes/Grid.svelte
+++ src/routes/Grid.svelte
@@ -40,6 +40,7 @@
         }}
         selected={a === i || b === i}
         found={found.includes(emoji)}
+        group={grid.indexOf(emoji) === i ? 'a' : 'b'}
         />
     {/each}
 </div>
@@ -51,5 +52,6 @@
         grid-template-rows: repeat(4,1fr);
         grid-gap: 0.5em;
         height: 100%;
+        perspective: 100vw;
     }
 </style>
--- src/routes/Square.svelte
+++ src/routes/Square.svelte
@@ -1,15 +1,25 @@
 <script lang="ts">
+    import { get_twemoji_url } from "./utils";
+    import { send } from './transition'
+
     export let emoji: string;
     export let selected: boolean;
     export let found: boolean;
+    export let group: 'a' | 'b';
 </script>
 
 
-<div class="square" class:flipped={selected}>
+<div class="square" class:flipped={selected || found}>
     <button on:click />
 
+    <div class="background" />
+
     {#if !found}
-        <span>{emoji}</span>
+        <img
+            out:send={{key: `${emoji}:${group}` }}
+            alt={emoji}
+            src={get_twemoji_url(emoji)} 
+        />
     {/if}
 </div>
 
@@ -18,21 +28,41 @@
         display: flex;
         justify-content: center;
         align-items: center;
-        background: yellow;
+        transform-style: preserve-3d;
+        transition: transform 0.4s;
     }
 
-    .flipped button {
-        background: red;
+    .flipped {
+        transform: rotateY(180deg);
     }
     
     button {
         position: absolute;
         width: 100%;
         height: 100%;
+        backface-visibility: hidden;
+        background: #eee;
+        border: 0;
+        border-radius: 1em;
+        font-size: inherit;
     }
 
-    span {
-        font-size: 5em;
-        pointer-events: none;
+    .background {
+        position: absolute;
+        background: white;
+        border: 0.5em solid #eee;
+        transform: rotateY(180deg);
+        backface-visibility: hidden;
+        width: 100%;
+        height: 100%;
+        border-radius: 1em;
     }
-</style>
+
+    img {
+        width: 6em;
+        height: 6em;
+        pointer-events: none;
+        transform: rotateY(180deg);
+        backface-visibility: hidden;
+    }
+</style> 
